792c0fa040c29c8105cdef62760c03ef9acccfa0
shitijrajpoot12-cyber <shitijrajpoot12@gmail.com>
2026-01-20 23:15:50 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,11 @@
+*/
+create database schemas
+  /*
+
+show databases;
+use sys;
+create database dataWareshouse;
+use dataWarehouse;
+create schema Bronze ;
+create schema Silver ;
+create schema Gold ;
